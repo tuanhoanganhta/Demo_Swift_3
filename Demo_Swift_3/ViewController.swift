685a0e2aeecf10405d2770e055a0b4e76d45f919
TUAN HOANG ANH TA <tuanhoanganhta@TUANs-MacBook-Pro.local>
2016-11-27 08:06:47 +0700

    Initial Commit

---
+++ Demo_Swift_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_Swift_3
+//
+//  Created by TUAN HOANG ANH TA on 11/27/16.
+//  Copyright © 2016 tuanhoanganhta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
